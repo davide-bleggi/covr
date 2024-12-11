0a0522086302cb99d736923271a8acd006427280
Davide Bleggi <davide.bleggi@digitronica.it>
2024-12-11 16:48:49 +0100

    rimossa il test con user per uso del db

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -1,21 +1,18 @@
-import {connectToDatabase} from "$lib/db/sequelize";
-import {User} from "$lib/db/schemas";
-
-export async function GET() {
-    try {
-        await connectToDatabase();
-
-        // Fetch all users
-        const users = await User.findAll();
-        console.log(users)
-        return new Response(JSON.stringify(users), {
-            status: 200,
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        return new Response('Error fetching users', { status: 500 });
-    }
-}
+// import {connectToDatabase} from "$lib/db/sequelize";
+// import {User} from "$lib/db/schemas";
+//
+// export async function GET() {
+//     try {
+//         await connectToDatabase();
+//         console.log(users)
+//         return new Response(JSON.stringify(users), {
+//             status: 200,
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         });
+//     } catch (error) {
+//         console.error('Error fetching users:', error);
+//         return new Response('Error fetching users', { status: 500 });
+//     }
+// }
