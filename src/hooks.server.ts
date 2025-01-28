c5e2bb78ccb8f6e5466caea890efc76da8a74538
Davide Bleggi <davide.bleggi@digitronica.it>
2025-01-28 14:27:32 +0100

    refactor(hooks.server): ristrutturazione della gestione WebSocket e autenticazione JWT
    Questo commit riorganizza il codice nel file `hooks.server.ts`, separando la logica di inizializzazione del WebSocket e mantenendo la funzionalità di autenticazione tramite JWT. È stata aggiunta la funzione `initWss()` per gestire le connessioni WebSocket, migliorando la leggibilità e la manutenibilità del codice.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -8,6 +8,40 @@ let wss: WebSocketServer;
 
 const SECRET = process.env.JWT_SECRET || 'mysecret';
 
+function initWss() {
+	if (!wss) {
+		let lastChecked = new Date();
+
+		wss = new WebSocketServer({ port: 8080 });
+
+		wss.on('connection', (ws) => {
+			console.log('Client connected');
+
+			// Send a message to the client when connected
+			ws.send(JSON.stringify({ message: 'Connected to WebSocket server!' }));
+
+			// Notify the client of a database update
+			setInterval(async () => {
+				// ws.send(JSON.stringify({ update: true }));
+				const changes = await prisma.automaticTest.findMany({
+					where: {
+						executionDate: {
+							gt: lastChecked
+						}
+					}
+				});
+
+				if (changes.length > 0) {
+					ws.send('change deteced');
+				}
+
+			}, 30000); // Replace with real DB notification
+		});
+	}
+}
+
+initWss();
+
 export const handle: Handle = async ({ event, resolve }) => {
 	console.log('hook handle');
 	// Attempt to read the JWT from cookies
@@ -26,38 +60,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 			};
 			console.log('user saved');
 		} catch (e) {
-			event.locals.user = undefined
+			event.locals.user = undefined;
 		}
-	}
-
-	if (!wss) {
-		let lastChecked = new Date();
-		wss = new WebSocketServer({ port: 8080 });
-
-		wss.on('connection', (ws) => {
-			console.log('Client connected');
-
-			// Send a message to the client when connected
-			ws.send(JSON.stringify({ message: 'Connected to WebSocket server!' }));
-
-			// Notify the client of a database update
-			setInterval(async () => {
-				// ws.send(JSON.stringify({ update: true }));
-				const changes = await prisma.automaticTest.findMany({
-					where: {
-						executionDate: {
-							gt: lastChecked,
-						},
-					},
-				});
-				console.log('changes: ', changes)
-
-				if (changes.length > 0) {
-					ws.send('change deteced')
-				}
-
-			}, 30000); // Replace with real DB notification
-		});
 	}
 
 	return resolve(event);
