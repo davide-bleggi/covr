8254f3eef12a150a43b2658a963377f1babcc0cc
Davide Bleggi <davide.bleggi@digitronica.it>
2025-02-05 13:49:49 +0100

    fix(websocket): previene errore in inizializzazione applicativo con porta websocket occupata.
    Aggiunta la funzione di inizializzazione per il WebSocketServer nel file hooks.server.ts. La connessione dei client ora è gestita correttamente e viene notificato quando ci sono cambiamenti nel database. Gestita anche l'eccezione durante l'assegnazione del server.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,40 +3,46 @@ import { type Handle, redirect } from '@sveltejs/kit';
 import jwt from 'jsonwebtoken';
 import prisma from '$lib/prisma';
 import { WebSocketServer } from 'ws';
+import { DATABASE_URL } from '$env/static/private';
+import { sequence } from '@sveltejs/kit/hooks';
 
 let wss: WebSocketServer;
 
 const SECRET = process.env.JWT_SECRET || 'mysecret';
 
+// INIT WEBSOCKET (non serve all'hook vero e proprio è solo un posto neutrale dove poter inizializzare la websocket)
 function initWss() {
 	if (!wss) {
 		let lastChecked = new Date();
+		try {
+			wss = new WebSocketServer({ port: 8080 });
 
-		wss = new WebSocketServer({ port: 8080 });
+			wss.on('connection', (ws) => {
+				console.log('Client connected');
 
-		wss.on('connection', (ws) => {
-			console.log('Client connected');
+				// Send a message to the client when connected
+				ws.send(JSON.stringify({ message: 'Connected to WebSocket server!' }));
 
-			// Send a message to the client when connected
-			ws.send(JSON.stringify({ message: 'Connected to WebSocket server!' }));
-
-			// Notify the client of a database update
-			setInterval(async () => {
-				// ws.send(JSON.stringify({ update: true }));
-				const changes = await prisma.automaticTest.findMany({
-					where: {
-						executionDate: {
-							gt: lastChecked
+				// Notify the client of a database update
+				setInterval(async () => {
+					// ws.send(JSON.stringify({ update: true }));
+					const changes = await prisma.automaticTest.findMany({
+						where: {
+							executionDate: {
+								gt: lastChecked
+							}
 						}
+					});
+
+					if (changes.length > 0) {
+						ws.send('change deteced');
 					}
-				});
 
-				if (changes.length > 0) {
-					ws.send('change deteced');
-				}
-
-			}, 30000); // Replace with real DB notification
-		});
+				}, 30000); // Replace with real DB notification
+			});
+		}catch(err){
+			console.log(err)
+		}
 	}
 }
 
