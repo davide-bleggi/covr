5a627ccd583b2918c552d69a78c20c8ec1f45341
Davide Bleggi <davide.bleggi@digitronica.it>
2025-02-10 16:25:57 +0100

    refactor(feature-item): migliorata la struttura del componente
    Modificata la struttura HTML del componente feature-item in Svelte per una migliore leggibilità e gestione degli elementi. Aggiunta una classe per il link e riorganizzati i contenuti interni per una presentazione più chiara delle informazioni.

--- src/routes/project/[code]/feature-item.svelte
+++ src/routes/project/[code]/feature-item.svelte
@@ -8,16 +8,19 @@
 	} = $props();
 
 </script>
-
-<a href={`/features/${feature.id}`}>
-<div class="flex flex-row w-full gap-4 justify-between items-center">
-	<div class="flex items-center gap-2 w-full">
-		<Button variant="ghost" class="flex justify-start w-full overflow-ellipsis py-6">
-			<div class="flex flex-col items-start font-normal ">
-				<h2 class="font-bold">{feature.name}</h2>
-				<span class=" flex justify-start">{feature.description}</span>
-			</div>
-		</Button>
+<a class="w-full" href={`/features/${feature.id}`}>
+	<div class="flex flex-row w-full gap-4 justify-between items-center">
+		<div class="flex items-center  w-full">
+			<Button variant="ghost" class="w-full py-6 px-4">
+				<div class="flex justify-start w-full gap-2">
+					<span>FEAT<br />{feature.id}</span>
+					<div class="flex flex-1 flex-col w-full items-start font-normal overflow-hidden">
+						<h2 class="font-bold">{feature.name}</h2>
+						<div
+							class="truncate w-full text-left">{feature.description}</div>
+					</div>
+				</div>
+			</Button>
+		</div>
 	</div>
-</div>
 </a>
