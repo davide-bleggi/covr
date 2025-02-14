330e3eb31de42f85f5ad9d60598a55c7e83722f7
Davide Bleggi <davide.bleggi@digitronica.it>
2025-02-14 12:52:47 +0100

    feat(project): aggiunta di Resizable e ScrollArea nella pagina del progetto
    Implementata una nuova struttura per il layout della pagina del progetto utilizzando i componenti Resizable e ScrollArea. Questo migliora l'esperienza utente consentendo una gestione più flessibile delle dimensioni dei pannelli e una navigazione scorrevole tra le versioni del progetto.

--- src/routes/project/[code]/+page.svelte
+++ src/routes/project/[code]/+page.svelte
@@ -6,6 +6,8 @@
 	import type { PageData } from './$types.js';
 	import { VersionDialog, VersionItem } from './index';
 	import { Progress } from '$lib/components/ui/progress';
+	import * as Resizable from '$lib/components/ui/resizable/index.js';
+	import { ScrollArea } from '$lib/components/ui/scroll-area';
 
 
 	let { data } = $props();
@@ -18,30 +20,51 @@
 <VersionDialog bind:open={openVersionDialog} formToValidate={data.versionForm} versions={data.versions}>
 </VersionDialog>
 
-<div class="flex w-full flex-col">
-	<div class="flex flex-row justify-between w-full p-4  h-fit items-center gap-4">
-		<div class="flex flex-1 justify-between">
-			<h1 class="font-bold text-lg">
-				{data.project.name.toUpperCase()}
-			</h1>
-			<div class="flex flex-col">
-				<span class="opacity-70 text-sm">Copertura features</span>
-				<div class="flex flex-row w-[200px] items-center gap-2">
-					<Progress value={data.project.coverage} class="h-2" />
-					<span>{data.project.coverage}%</span>
+<Resizable.PaneGroup
+	direction="horizontal"
+	class="min-h-[200px] h-full w-full rounded-lg flex-1 flex flex-col"
+>
+	<Resizable.Pane class="flex flex-col" defaultSize={70} minSize={30}>
+		<div class="flex h-full w-full flex-col">
+			<div class="flex flex-row justify-between w-full p-4  h-fit items-center gap-4">
+				<div class="flex flex-1 justify-between">
+					<h1 class="font-bold text-lg">
+						{data.project.name.toUpperCase()}
+					</h1>
+					<div class="flex flex-col">
+						<span class="opacity-70 text-sm">Copertura features</span>
+						<div class="flex flex-row w-[200px] items-center gap-2">
+							<Progress value={data.project.coverage} class="h-2" />
+							<span>{data.project.coverage}%</span>
+						</div>
+					</div>
+				</div>
+				<Button variant="outline" class="" onclick={()=>openProjectDialog=true}>
+					<PencilIcon />
+				</Button>
+			</div>
+
+			<div class="flex flex-col flex-1 h-0 min-h-0  w-full max-w-[700px] mx-auto gap-2">
+				<Button class="p-4 w-full" variant="outline" onclick={()=>openVersionDialog=true}>
+					Aggiungi Versione
+				</Button>
+				<div class="flex-1 flex h-0 min-h-0  mb-2 w-full">
+					<ScrollArea class="w-full overflow-auto">
+						<ul class="w-full relative">
+							{#each data.versions as version}
+								<li class="w-full">
+									<VersionItem {version} customers={data.customers}></VersionItem>
+								</li>
+							{/each}
+						</ul>
+					</ScrollArea>
 				</div>
 			</div>
+
 		</div>
-		<Button variant="outline" class="" onclick={()=>openProjectDialog=true}>
-			<PencilIcon />
-		</Button>
-	</div>
-	<div class="flex flex-col w-full max-w-[700px] mx-auto gap-2 justify-center items-center">
-		<Button class="p-4 w-full" variant="outline" onclick={()=>openVersionDialog=true}>
-			Aggiungi Versione
-		</Button>
-		{#each data.versions as version}
-			<VersionItem {version} customers={data.customers}></VersionItem>
-		{/each}
-	</div>
-</div>
+	</Resizable.Pane>
+	<Resizable.Handle />
+	<Resizable.Pane defaultSize={30} minSize={30}>
+
+	</Resizable.Pane>
+</Resizable.PaneGroup>
