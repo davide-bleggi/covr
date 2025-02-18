4da95b66a99acd8f40eb29bc76db8f3ac639c4b4
Davide Bleggi <davide.bleggi@digitronica.it>
2025-02-18 16:36:53 +0100

    feat(accordion): migliora l'aspetto del trigger dell'accordion
    Aggiornata la classe del componente AccordionPrimitive.Header per includere il modificatore "group" e migliorata la gestione delle classi per il ChevronDown. Aggiunta la rotazione del chevron al cambio di stato dell'accordion.

--- src/lib/components/ui/accordion/accordion-trigger.svelte
+++ src/lib/components/ui/accordion/accordion-trigger.svelte
@@ -12,16 +12,17 @@
 	}: WithoutChild<AccordionPrimitive.TriggerProps> & {
 		level?: AccordionPrimitive.HeaderProps["level"];
 	} = $props();
+
 </script>
 
-<AccordionPrimitive.Header {level} class="flex w-full gap-2">
-	<div class="h-full flex items-center">
-	 <ChevronDown class="size-4 shrink-0 transition-transform duration-200" />
+<AccordionPrimitive.Header {level} class="flex w-full gap-2 group">
+	<div class={cn(`h-full flex items-center`)}>
+		<ChevronDown class={cn(`size-4 shrink-0 transition-transform duration-200 group-data-[state=open]:rotate-180`)} />
 	</div>
 	<AccordionPrimitive.Trigger
 		bind:ref
 		class={cn(
-			"flex flex-1 w-full items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
+			`flex flex-1 w-full items-center justify-between py-4 font-medium transition-all hover:underline`,
 			className
 		)}
 		{...restProps}
